9c401f291a759293b64b713edb0f5f19501b70a3
the-kuki <knalborczyk2003@gmail.com>
2021-11-20 13:47:59 +0100

    Possibility to add different types of logic gates. Possibility to move logic gates.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +16,82 @@ using System.Windows.Shapes;
 
 namespace Symulator_ukladow_logicznych
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        UIElement dragObj = null;
+        Point offset;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void canvasPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.dragObj == null)
+            {
+                return;
+            }
+
+            var posObj = e.GetPosition(sender as IInputElement);
+
+            Canvas.SetLeft(this.dragObj, posObj.X - this.offset.X);
+            Canvas.SetTop(this.dragObj, posObj.Y - this.offset.Y);
+        }
+
+        private void canvasPreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.dragObj = null;
+            this.canvas.ReleaseMouseCapture();
+        }
+
+        private void createGate(object sender, RoutedEventArgs e)
+        {
+            Rectangle rect = new Rectangle();
+
+            switch ((string)((Button)sender).Tag)
+            {
+                case "btnAND":
+                    rect.Fill = Brushes.Red;
+                    rect.Tag = "AND";
+                    break;
+                case "btnNOT":
+                    rect.Fill = Brushes.LightGreen;
+                    rect.Tag = "NOT";
+                    break;
+                case "btnNAND":
+                    rect.Fill = Brushes.Yellow;
+                    rect.Tag = "NAND";
+                    break;
+                case "btnOR":
+                    rect.Fill = Brushes.Purple;
+                    rect.Tag = "OR";
+                    break;
+                case "btnXOR":
+                    rect.Fill = Brushes.DarkGreen;
+                    rect.Tag = "XOR";
+                    break;
+            }
+            rect.Width = 100;
+            rect.Height = 50;
+
+            Canvas.SetLeft(rect, 350);
+            Canvas.SetTop(rect, 175);
+
+            rect.PreviewMouseDown += rectPreviewMouseDown;
+
+            canvas.Children.Add(rect);
+        }
+
+        private void rectPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.dragObj = sender as UIElement;
+
+            this.offset = e.GetPosition(this.canvas);
+            this.offset.X -= Canvas.GetLeft(this.dragObj);
+            this.offset.Y -= Canvas.GetTop(this.dragObj);
+
+            this.canvas.CaptureMouse();
         }
     }
 }
